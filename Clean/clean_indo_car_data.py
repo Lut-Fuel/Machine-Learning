77e36bc0e671505d36fd11fac906a3b4cb6dc46a
Ziyad <ziyadrff@gmail.com>
2023-12-19 13:54:07 +0700

    Adding script for SQL and also cleaning data that is not in format

--- Clean/clean_indo_car_data.py
+++ Clean/clean_indo_car_data.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+from typing import Optional
+from sqlmodel import Field, SQLModel, create_engine, Session
 
 #Read the data file
 df = pd.read_csv('Dataset/Raw Data/Mobil Indonesia.csv')
@@ -160,6 +162,7 @@ df = df.drop(['Consumption_L_per_100_km', 'Distance_km', 'Mixed_Fuel_Consumption
 df.rename(columns={'Consumption_km_per_L': 'Mixed_Fuel_Consumption_per_100_km_l'}, inplace=True)
 df['Mixed_Fuel_Consumption_per_100_km_l'] = df['Mixed_Fuel_Consumption_per_100_km_l'].round(1)
 
+df.dropna(inplace=True)
 
 print(df.info())
 print('')
@@ -176,3 +179,49 @@ df['Car Name'] = pd.concat([df['Maker'], df['Model']], axis=1).agg(' '.join, axi
 
 #Saving the database
 df.to_csv('Dataset/database_indo.csv', index=False)
+
+#Uploading database into SQL
+class Car(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    maker: str
+    model: str
+    number_of_cylinders: int
+    engine_type: int
+    engine_horse_power: float
+    engine_horse_power_rpm: int
+    transmission: int
+    fuel_tank_capacity: int
+    acceleration_0_to_100_km: float
+    max_speed_km_per_hour: int
+    fuel_grade: int
+    year: int
+    type_of_car: int
+    car_name: str
+
+DATABASE_URL = "postgresql://postgres:lutfuel123semangat@34.101.191.219:5432/postgres"
+engine = create_engine(DATABASE_URL)
+SQLModel.metadata.create_all(engine)
+
+with Session(engine) as session:
+    for index, row in df.iterrows():
+        try:
+            car = Car(
+                maker=row["Maker"],
+                model=row["Model"],
+                number_of_cylinders=row["Number_of_Cylinders"],
+                engine_type=row["Engine_Type"],
+                engine_horse_power=row["Engine_Horse_Power"],
+                engine_horse_power_rpm=row["Engine_Horse_Power_RPM"],
+                transmission=row["Transmission"],
+                fuel_tank_capacity=row["Fuel_Tank_Capacity"],
+                acceleration_0_to_100_km=row["Acceleration_0_to_100_Km"],
+                max_speed_km_per_hour=row["Max_Speed_Km_per_Hour"],
+                fuel_grade=row["Fuel_Grade"],
+                year=row["Year"],
+                type_of_car=row["Type_of_Car"],
+                car_name=row["Car Name"],
+            )
+            session.add(car)
+            session.commit()
+        except:
+            pass
